556e41a17f44bca2a27c9a39e4cf94998a040342
harshpatil01 <55093194+harshpatil01@users.noreply.github.com>
2024-01-03 23:51:42 +0100

    Add files via upload

---
+++ Binary search(normal)/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 0
+#define FAILURE -1
+#define ARRAYSIZE 25
+#define NOELEMENT -2
+
+int binarysearchiterative(int arr[], int left, int right, int data);
+
+int binarysearchrecursive(int arr[], int left, int right, int data);
